408f01fa31edd31594748c5d88652b303fd60645
YuryKozachek <117518443+YuryKozachek@users.noreply.github.com>
2022-11-21 20:41:25 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,10 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int a = 5;
-        int b = 3;
+        int a = 10;
+        int b = 30;
         int c = a * b;
-        System.out.println(c + " Ответ");
+        System.out.println(c + " Ответ новый");
 
     }
 }
