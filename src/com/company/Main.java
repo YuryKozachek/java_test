6027a80054116f61d84b41ffac71a01ee5612281
Yury Kozachek <show_@mail.ru>
2022-11-21 20:38:21 +0300

    java_commit3

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int a = 5;
+        int b = 3;
+        int c = a * b;
+        System.out.println(c + " Ответ");
+
+    }
+}
